b4c3e2572ebd8bc4857bb406ff27525feab8be41
latteisacat <dragonldh0907@gmail.com>
2023-02-04 21:22:46 +0900

    2023/02/04

--- baekjoon_2579.py
+++ baekjoon_2579.py
@@ -6,6 +6,7 @@ for i in range(user_input):
     stair.append(int(input()))
 
 max_sum = list()
+
 max_sum.append(stair[0])
 if user_input > 1:
     max_sum.append(max(stair[0]+stair[1], stair[1]))
@@ -15,4 +16,4 @@ if user_input > 2:
 for i in range(3, user_input):
     max_sum.append(max(max_sum[i-2]+stair[i], max_sum[i-3]+stair[i-1]+stair[i]))
 
-print(max_sum[user_input-1])
+print(max(max_sum))
